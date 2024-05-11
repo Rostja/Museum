3acc77327cbe15dad8ab2a2739a3383b68eb92b9
Rostja <rasto.dorcak@gmail.com>
2024-05-11 22:31:40 +0200

    Step 8

--- Package/Museum.java
+++ Package/Museum.java
@@ -5,26 +5,28 @@ import java.util.Random;
 
 public class Museum {
     public static void main(String[] args) {
-
-        int currentMonth = Calendar.getInstance().get(Calendar.MONTH);
+        int currentMonth = Calendar.getInstance().get(Calendar.MONTH) + 1; // Přičtení 1, protože měsíce jsou indexovány od 0
         int currentDay = Calendar.getInstance().get(Calendar.DAY_OF_MONTH);
         int daysOfMonth = Calendar.getInstance().getActualMaximum(Calendar.DAY_OF_MONTH);
+        int totalTickets = 0;
+        int totalReservedTickets = 0;
 
-
-        for (int day = currentDay; day <= daysOfMonth; day++){
-            int Reserved = generateRandomNumber(0,10);
-            double percentageOfDay = (((double)Reserved / 10) * 100);
-            System.out.print ( day + currentMonth + " : " + Reserved + " zarezervovanych listkov;  " );
-            System.out.print(" Pocet volnych listkov : " + (10 - Reserved) + "  " );
-            System.out.println(" Percentualne vytazenie na den : " + percentageOfDay + " %" );
-
+        for (int day = currentDay; day <= daysOfMonth; day++) {
+            int Reserved = generateRandomNumber(0, 10);
+            totalReservedTickets += Reserved;
+            totalTickets += 10;
+            double percentageOfDay = (((double) Reserved / 10) * 100);
+            System.out.print(day + "/" + currentMonth + " : " + Reserved + " zarezervovanych listkov;  ");
+            System.out.print(" Pocet volnych listkov : " + (10 - Reserved) + "  ");
+            System.out.println(" Percentualne vytazenie na den : " + percentageOfDay + " %");
         }
 
-        System.out.println("Percentualne vytazenie na mesiac : ");
-
+        double percentageOfMonth = (((double) totalReservedTickets / totalTickets) * 100);
+        System.out.println("Percentualne vytazenie za mesiac : " + percentageOfMonth + " %");
     }
-    public static int generateRandomNumber(int min,int max) {
+
+    public static int generateRandomNumber(int min, int max) {
         Random random = new Random();
-        return random.nextInt(max - min + 1);
+        return random.nextInt(max - min + 1) + min;
     }
 }
