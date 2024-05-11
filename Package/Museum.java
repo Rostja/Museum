6668e0dcbf00f2b5c0343025f00b5342f15f8e6a
Rostja <rasto.dorcak@gmail.com>
2024-05-11 21:52:21 +0200

    Step 3

--- Package/Museum.java
+++ Package/Museum.java
@@ -1,12 +1,17 @@
 package Package;
 
+import java.util.Calendar;
 import java.util.Random;
 
 public class Museum {
     public static void main(String[] args) {
 
+        int currentMonth = Calendar.getInstance().get(Calendar.MONTH);
+        int
+
         for (int i = 0, i <= dayOfMonth, i++){
             int Reserved = generateRandomNumber(0,10);
+            System.out.println( day + currentMonth + " : " + Reserved + " zarezervovanych listkov" );
         }
 
 
