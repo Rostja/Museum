db37e7fa0f749143833fc0cae870a39b2c929b39
Rostja <rasto.dorcak@gmail.com>
2024-05-11 21:56:26 +0200

    Step 5

--- Package/Museum.java
+++ Package/Museum.java
@@ -10,7 +10,7 @@ public class Museum {
         int currentDay = Calendar.getInstance().get(Calendar.DAY_OF_MONTH);
         int daysOfMonth = Calendar.getInstance().getActualMaximum(Calendar.DAY_OF_MONTH);
 
-        for (int i = 0, i <= daysOfMonth, i++){
+        for (int day = currentDay; day <= daysOfMonth; day++){
             int Reserved = generateRandomNumber(0,10);
             System.out.println( day + currentMonth + " : " + Reserved + " zarezervovanych listkov" );
         }
